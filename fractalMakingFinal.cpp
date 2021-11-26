48cfda6b55c269cb88bc7a7da16a1f9801d0e9a7
Michal Lüley <55835336+michal-luley@users.noreply.github.com>
2021-11-26 12:19:41 +0100

    hotovo pre refaktoringom

--- fractalMakingFinal.cpp
+++ fractalMakingFinal.cpp
@@ -10,8 +10,10 @@ using namespace cv;
 using namespace std;
 
 #define MAX_ITER 1000
-#define WIDTH 900
-#define HEIGHT 900
+#define WIDTH 10000
+#define HEIGHT 10000
+
+Mat image_main;
 
 float scale(float A, float A2, float Min, float Max)
 {
@@ -38,85 +40,76 @@ int mandelbrot(double x_orig, double y_orig) {
 	return iteration;
 }
 
-void setColorsOfPixel(vector <vector<uint32_t> >& pole, int row_start, int row_end) {
+void setColorsOfPixel(vector <uint32_t> & pole, int row_start, int row_end) {
 	int x, y;
 	int iter;
 	int scaled_value;
 
-	for (x = 0; x < WIDTH; x++) {
-		for (y = row_start; y < row_end; y++) {
+	for (x = row_start; x < row_end; x++) {
+		for (y = 0; y < WIDTH; y++) {
 			iter = mandelbrot(x, y);
 			scaled_value = scale(iter, MAX_ITER, 255, 0);
-			pole[y][x] = scaled_value;
+			pole[(x-row_start)*WIDTH+y] = scaled_value;
 		}
 	}
-
 }
 
-void matrcv() {
+Mat matrcv() {
 	MPI_Status status;
+	MPI_Request recv_req;
 	int id_source, ntasks;
 	int row_start, row_end, count;
-	int all;
 
 	MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
 
-	std::vector <vector<uint32_t> >received(HEIGHT, vector<uint32_t>(WIDTH));
-	ntasks--;
-	all = ntasks;
+	int number_of_rows_per_send = HEIGHT / ntasks;
 
-	//receiving image
-	//while (all != 0) {
 
-	for (int i = 0; i < 300; i++) {
-		MPI_Recv(&received[i][0], WIDTH, MPI_UINT32_T, 1, 0, MPI_COMM_WORLD, &status);
+	std:: vector <vector<uint32_t> >received(ntasks-1, vector<uint32_t> (number_of_rows_per_send*WIDTH));
+	
+
+	Mat image = Mat::zeros(WIDTH, HEIGHT, CV_8UC3);
+	Mat image2 = Mat::zeros(WIDTH, HEIGHT, CV_8UC3);
+	int job_done = 0;
+	for (int source_id = 0; source_id < (ntasks-1); source_id++) {
+		vector<uint32_t> received_helper(number_of_rows_per_send * WIDTH);
+		MPI_Irecv(&received_helper[0], number_of_rows_per_send * WIDTH, MPI_UINT32_T, source_id+1, MPI_ANY_TAG, MPI_COMM_WORLD, &recv_req);
+
 		
-	}
-	for (int i = 300; i < 600; i++) {
-		MPI_Recv(&received[i][0], WIDTH, MPI_UINT32_T, 2, 0, MPI_COMM_WORLD, &status);
+		if (job_done == 0) {
+			row_start = number_of_rows_per_send * (ntasks - 1);
+			row_end = HEIGHT;
+			int rows_local = row_end - row_start;
 
-	}
-	for (int i = 600; i < 900; i++) {
-		MPI_Recv(&received[i][0], WIDTH, MPI_UINT32_T, 3, 0, MPI_COMM_WORLD, &status);
+			vector<uint32_t> received_helper_local(rows_local * WIDTH);
+			setColorsOfPixel(received_helper_local, row_start, row_end);
+			for (int i = 0; i < rows_local; i++) {
+				for (int j = 0; j < WIDTH; j++) {
+					for (int k = 0; k < 3; k++) {
+						image.at<Vec3b>(Point(i + (ntasks-1) * rows_local, j)).val[k] = received_helper_local[i * WIDTH + j];
+					}
+				}
+			}
+			job_done = 1;
+		}
+		
+		MPI_Wait(&recv_req, &status);
 
-	}
-	Mat image = Mat::zeros(WIDTH, HEIGHT, CV_8UC3);
-	for (int i = 0; i < WIDTH; i++) {
-		for (int j = 0; j < HEIGHT; j++) {
-			image.at<Vec3b>(Point(i, j)).val[0] = received[i][j];
-			image.at<Vec3b>(Point(i, j)).val[1] = received[i][j];
-			image.at<Vec3b>(Point(i, j)).val[2] = received[i][j];
+		#pragma omp parallel shared(image)
+		{
+
+		#pragma omp for
+			for (int i = 0; i < number_of_rows_per_send; i++) {
+				for (int j = 0; j < WIDTH; j++) {
+					for (int k = 0; k < 3; k++) {
+						image.at<Vec3b>(Point(i + source_id * number_of_rows_per_send, j)).val[k] = received_helper[i * WIDTH + j];
+					}
+				}
+			}
 		}
 	}
-	imshow("pokus", image);
-	waitKey(0);
-
-	
-
-
-	
-
-
-	//id_source = status.MPI_SOURCE;
-	//row_start = (double)id_source / (double)ntasks * HEIGHT;
-	//row_end = ((double)id_source + 1.0) / (double)ntasks * HEIGHT;
-	//all--;
-
-	//}
 	cout << "donereceiver";
-
-	/*
-	Mat image = Mat::zeros(WIDTH, HEIGHT, CV_8UC3);
-	for (int i = 0; i < WIDTH; i++) {
-		for (int j = 0; j < HEIGHT; j++) {
-			image.at<Vec3b>(Point(i, j)).val[0] = received[0][j];
-			image.at<Vec3b>(Point(i, j)).val[1] = received[0][j];
-			image.at<Vec3b>(Point(i, j)).val[2] = received[0][j];
-		}
-	}
-	imshow("pokus", image);
-	waitKey(0);
-	*/
+	return image;
 }
 
 void matsnd() {
@@ -124,39 +117,33 @@ void matsnd() {
 	int id, ntasks;
 	int row_start, row_end;
 
-	std::vector <vector<uint32_t> >original(HEIGHT, vector<uint32_t>(WIDTH));
-
 	//important MPI stuff
 	MPI_Status status;
 	MPI_Comm_rank(MPI_COMM_WORLD, &id);
 	MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
-	ntasks--;
 	id--;
 
-	//define own rows to process;
-	row_start = (double)id / (double)ntasks * HEIGHT;
-	row_end = ((double)id + 1.0) / (double)ntasks * HEIGHT;
+	//define esentials of process;
+	int number_of_rows_per_send = HEIGHT / ntasks;
+	row_start = id * number_of_rows_per_send;
+	row_end = (id+1) * number_of_rows_per_send;
+
+	std::vector<uint32_t> original(number_of_rows_per_send * WIDTH);
+
+	//computation
 	setColorsOfPixel(original, row_start, row_end);
 
-
-	for (int i = id*300; i < id*300+300; i++) {
-		MPI_Send(&original[i][0], WIDTH, MPI_UINT32_T, 0, 0, MPI_COMM_WORLD);
-	}
-	
-	
-
-	
+	//sending
+	MPI_Send(&original[0], number_of_rows_per_send * WIDTH, MPI_UINT32_T, 0, id, MPI_COMM_WORLD);
 }
+
 
 int main(int argc, char** argv)
 {
+	Mat imageResized;
 	int err, np, me, i, j;
 	MPI_Status status;
-
-	Mat image = Mat::zeros(WIDTH, HEIGHT, CV_8UC3);
-
-	std::vector <vector<uint32_t> >received(10000, vector<uint32_t>(10000));
-
+	
 	err = MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &me);
 	MPI_Comm_size(MPI_COMM_WORLD, &np);
@@ -175,7 +162,10 @@ int main(int argc, char** argv)
 	}
 
 	if (me == 0) {
-		matrcv();
+		image_main = matrcv();
+		cv::resize(image_main, image_main, cv::Size(900, 900));
+		imshow("MandelBrot", image_main);
+		waitKey(0);
 	}
 
 	else {
